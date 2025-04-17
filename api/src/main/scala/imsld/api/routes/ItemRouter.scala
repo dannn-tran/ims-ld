fc1308fd4ec7b08e2d0aa3ae5b31c6f6d7492780
picas9dan <picasdan99@gmail.com>
2025-04-17 22:19:59 +0800

    Refactor router

--- api/src/main/scala/imsld/api/routes/ItemRouter.scala
+++ api/src/main/scala/imsld/api/routes/ItemRouter.scala
@@ -4,45 +4,11 @@ import cats.effect.Concurrent
 import cats.syntax.all.*
 import io.circe
 import io.circe.generic.auto.*
-import org.http4s.circe.CirceEntityCodec.*
-import org.http4s.dsl.Http4sDsl
-import org.http4s.dsl.io.*
-import org.http4s.{HttpRoutes, Request, Response}
 
-import imsld.api.routes.common.*
 import imsld.api.services.ItemService
-import imsld.model.{ItemNew, PagingRequest}
+import imsld.model.ItemNew
+import imsld.model.Item
+import imsld.model.ItemPartial
 
 final class ItemRouter[F[_]: Concurrent](service: ItemService[F])
-    extends Http4sDsl[F] {
-  val routes: HttpRoutes[F] =
-    HttpRoutes.of[F] {
-      case GET -> Root :? PageNumberQueryParamMatcher(
-            pageNum
-          ) +& PageSizeQueryParamMatcher(pageSize) =>
-        for {
-          data <- service.getAll(
-            PagingRequest(pageNum, pageSize)
-          )
-          resp <- Ok(data)
-        } yield resp
-
-      case GET -> Root / IntVar(id) =>
-        for {
-          itemOpt <- service.getOneById(id)
-          resp <- itemOpt.fold(NotFound()) { item => Ok(item) }
-        } yield resp
-
-      case req @ POST -> Root =>
-        req
-          .attemptAs[List[ItemNew]]
-          .foldF(
-            err => BadRequest(err.toString),
-            docs =>
-              for {
-                ids <- service.insertMany(docs)
-                resp <- Created(ids)
-              } yield resp
-          )
-    }
-}
+    extends RouterBase(service)
---
+++ api/src/main/scala/imsld/api/routes/RouterBase.scala
@@ -0,0 +1,61 @@
+package imsld.api.routes
+
+import cats.effect.Concurrent
+import cats.syntax.all.*
+import io.circe
+import io.circe.generic.auto.*
+import org.http4s.circe.CirceEntityCodec.*
+import org.http4s.dsl.Http4sDsl
+import org.http4s.dsl.io.*
+import org.http4s.{HttpRoutes, Request, Response}
+
+import imsld.api.routes.common.*
+import imsld.model.PagingRequest
+import imsld.api.services.ServiceBase
+
+abstract class RouterBase[
+    F[_]: Concurrent,
+    T,
+    TNew,
+    TPartial,
+    Service <: ServiceBase[F, T, TNew, TPartial]
+](
+    service: Service
+)(using circe.Encoder[T], circe.Decoder[TNew], circe.Encoder[TPartial])
+    extends Http4sDsl[F] {
+  def routes: HttpRoutes[F] =
+    HttpRoutes.of[F](customRouteHandler `orElse` defaultRouteHandler)
+
+  def customRouteHandler: PartialFunction[Request[F], F[Response[F]]] =
+    PartialFunction.empty
+
+  val defaultRouteHandler: PartialFunction[Request[F], F[Response[F]]] = {
+    case GET -> Root :? PageNumberQueryParamMatcher(
+          pageNum
+        ) +& PageSizeQueryParamMatcher(pageSize) =>
+      for {
+        data <- service.getAll(
+          PagingRequest(pageNum, pageSize)
+        )
+        resp <- Ok(data)
+      } yield resp
+
+    case GET -> Root / IntVar(id) =>
+      for {
+        itemOpt <- service.getOneById(id)
+        resp <- itemOpt.fold(NotFound()) { item => Ok(item) }
+      } yield resp
+
+    case req @ POST -> Root =>
+      req
+        .attemptAs[List[TNew]]
+        .foldF(
+          err => BadRequest(err.toString),
+          docs =>
+            for {
+              ids <- service.insertMany(docs)
+              resp <- Created(ids)
+            } yield resp
+        )
+  }
+}
